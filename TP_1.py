7f2c6b06e3451af463f71ba8ba1abcb904714761
MaelleLG <maellelegavrian@outlook.fr>
2017-12-28 15:07:28 +0100

    TP1

--- TP_1.py
+++ TP_1.py
@@ -1,5 +1,26 @@
 import numpy as np 
-import panda as pd 
-import matplotlib.pyplot as plt 
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-house_data = pd.read_csv('C:\Users\Maelle\Documents\A3\MachineLearning\house.csv')
+house_data_raw = pd.read_csv('house.csv')
+house_data = house_data_raw[house_data_raw['loyer']<7000]
+
+#donnees
+plt.plot(house_data['surface'], house_data['loyer'], 'ro')
+
+#ensemble des données
+X = np.matrix([np.ones(house_data.shape[0]), house_data['surface']]).T
+#vecteur cible (montant du loyer)
+y = np.matrix(house_data['loyer']).T
+#estimateur
+theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+print(theta)
+
+#regression lineaire
+plt.plot([0,250],[theta.item(0),theta.item(0)+250*theta.item(1)])
+plt.show()
+
+
+
+
+
